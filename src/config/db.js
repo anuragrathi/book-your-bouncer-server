b715d5bfc756c955dc2ad444282a117779e978a3
Paramjeet Work <paramjeet.s@bookyourbouncer.com>
2025-04-23 18:02:49 +0530

    test3

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const { default: mongoose } = require("mongoose");
-
+// connection to db
 mondbUrl =
   "mongodb+srv://anuragrathi:kDGSLAShH6xVOTzJ@registrations.tlqyisv.mongodb.net/?retryWrites=true&w=majority&appName=REGISTRATIONS";
 const connectDb = () => {
