837d69f4b3f8d4b2d3635ddbe9eb29d4f7d00e6d
anuragrathi <anurag.r@bookyourbouncer.com>
2025-05-01 20:16:31 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,6 @@
 const { default: mongoose } = require("mongoose");
-<<<<<<< HEAD
 //connect to the database
 //enter in mondbUrl  "mongodb+srv://anuragrathi:kDGSLAShH6xVOTzJ@registrations.tlqyisv.mongodb.net/?retryWrites=true&w=majority&appName=REGISTRATIONS
-=======
->>>>>>> 1174869f64a4eec9365c62d0df18241bdb792972
 // connection to db
 mondbUrl =
   "******************************************************************";
