37ec395335e5ef862eca6631a2463077a424a4b4
Vaibhav Kumar <120007942+vaibhavnehwal@users.noreply.github.com>
2025-05-23 22:52:04 +0530

    ProductLine item sync with sfdc

--- src/controller/productLineItem.controller.js
+++ src/controller/productLineItem.controller.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const ProductLineItem = require("../models/productLineItem.model.js");
 
 const createProductLineItem = async (req, res) => {
@@ -21,7 +22,34 @@ const getAllProductLineItems = async (req, res) => {
   }
 };
 
+// Update an Productt by ID
+const updateproductLineItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Validate ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid Product ID" });
+    }
+
+    const updateproductLineItem = await productLineItem.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+
+    if (!updateproductLineItem) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json(updateproductLineItem);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
 module.exports = {
   createProductLineItem,
   getAllProductLineItems,
+  updateproductLineItem
 };
--- src/routes/productLineItem.route.js
+++ src/routes/productLineItem.route.js
@@ -4,5 +4,6 @@ const itemController = require("../controller/productLineItem.controller.js");
 
 router.post("/", itemController.createProductLineItem);
 router.get("/", itemController.getAllProductLineItems);
+router.put("/:id", productController.updateproductLineItem);
 
 module.exports = router;
